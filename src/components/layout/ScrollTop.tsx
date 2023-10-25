5e118c14947da5a6930fdb70d0b1eb078bd3eb87
ttiimmothy <dinosauli2006@gmail.com>
2023-10-25 05:19:58 -0400

    Feature: add link to scroll top button

--- src/components/layout/ScrollTop.tsx
+++ src/components/layout/ScrollTop.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import { IoChevronUpOutline } from "react-icons/io5";
 
@@ -23,32 +24,34 @@ const ScrollTop: React.FC = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
-    const anchor = (
-      (event.target as HTMLDivElement).ownerDocument || document
-    ).querySelector("#home");
+  // const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+  //   const anchor = (
+  //     (event.target as HTMLDivElement).ownerDocument || document
+  //   ).querySelector("#home");
 
-    if (anchor) {
-      anchor.scrollIntoView({
-        behavior: "smooth",
-        // block: "center",
-      });
-    }
-  };
+  //   if (anchor) {
+  //     anchor.scrollIntoView({
+  //       behavior: "smooth",
+  //       // block: "center",
+  //     });
+  //   }
+  // };
 
   return (
     <div className={`${scrolled ? "visible" : "invisible"}`}>
-      <div
-        className="fixed bottom-10 right-10 cursor-pointer"
-        onClick={handleClick}
-      >
-        <button
-          aria-label="scroll back to top"
-          className="outline-none bg-contact-button hover:bg-gray-200 w-12 h-12 rounded-full flex justify-center items-center"
+      <Link href="/">
+        <div
+          className="fixed bottom-10 right-10 cursor-pointer"
+          // onClick={handleClick}
         >
-          <IoChevronUpOutline size={20} className="dark:text-gray-50" />
-        </button>
-      </div>
+          <button
+            aria-label="scroll back to top"
+            className="outline-none bg-contact-button hover:bg-gray-200 w-12 h-12 rounded-full flex justify-center items-center"
+          >
+            <IoChevronUpOutline size={20} className="dark:text-gray-50" />
+          </button>
+        </div>
+      </Link>
     </div>
   );
 };
