090b517560eae93e2b40b964cab8a8a75c7f85a5
ttiimmothy <dinosauli2006@gmail.com>
2023-11-19 21:16:42 -0500

    feat: delete codes

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -33,7 +33,6 @@ const ContactSection: React.FC = () => {
       process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID &&
       process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
     ) {
-      // console.log(form.email);
       emailjs
         .send(
           process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
