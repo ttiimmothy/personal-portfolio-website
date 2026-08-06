f0a9e22bffe1838565a92956588a776b10f70f1a
Timothy <timothytimothytimo6@gmail.com>
2026-08-06 04:03:25 -0400

    fix: blog styling
    render the blog page until meta data is loaded
    
    click hyperlink in mdx to open new tab

--- src/components/posts/PostLayout.tsx
+++ src/components/posts/PostLayout.tsx
@@ -1,12 +1,22 @@
+"use client";
+
+import { useCallback, useState } from "react";
 import type { ReactNode } from "react";
 import AnimatedMDXContent from "./AnimatedMDXContent";
 import PostFooter from "./PostFooter";
 import PostHeader from "./PostHeader";
 
 export default function PostLayout({ children }: { children: ReactNode }) {
+  const [metadataLoaded, setMetadataLoaded] = useState(false);
+  const handleMetadataLoaded = useCallback(() => setMetadataLoaded(true), []);
+
   return (
-    <article className="post-content prose prose-slate mx-auto min-h-screen max-w-3xl px-5 pb-20 pt-28 dark:prose-invert md:px-0">
-      <PostHeader />
+    <article
+      className={`post-content prose prose-slate prose-headings:font-medium mx-auto min-h-screen max-w-3xl px-5 pb-20 pt-28 dark:prose-invert md:px-0${
+        metadataLoaded ? "" : " invisible"
+      }`}
+    >
+      <PostHeader onMetadataLoaded={handleMetadataLoaded} />
       <AnimatedMDXContent>{children}</AnimatedMDXContent>
       <PostFooter />
     </article>
